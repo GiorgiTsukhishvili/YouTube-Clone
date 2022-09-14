f91740ae59d1b278ad3cec8f9d1dfccb3c454a15
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-14 15:33:23 +0400

    Creating starting file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="flex flex-wrap justify-center items-center m-10">App</div>
+    <div className="bg-black">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </div>
   );
 };
 
