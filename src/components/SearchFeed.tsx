acca2b66a2a94d4293948d76f3cf2f2ca0fa4b32
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-15 16:42:40 +0400

    Search done

--- src/components/SearchFeed.tsx
+++ src/components/SearchFeed.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Videos } from "./feed";
+import { fetchFromAPI } from "../utils/fetchFromApi";
+import { FetchedDataVideos } from "../interfaces/fetchedDataInterface";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [error, setError] = useState<boolean>(false);
+  const [videos, setVideos] = useState<FetchedDataVideos[]>([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const fetched = await fetchFromAPI(
+          `search?part=snippet&q=${searchTerm}`
+        );
+        setVideos(fetched.items);
+      } catch (err) {
+        setError(true);
+      }
+    };
+
+    getData();
+  }, [searchTerm]);
+
+  return (
+    <div className="p-2 overflow-y-auto h-[94vh] ">
+      <h1 className=" font-bold mb-2 text-white text-[28px]">
+        Search Results for: <span className="text-[#F31503]">{searchTerm}</span>
+      </h1>
+
+      {error ? (
+        <h1 className="text-white text-3xl absolute top-[40%] left-[40%] text-center">
+          404 It Was Not possible to Get Videos, <br />
+          Try Different Search
+        </h1>
+      ) : (
+        <Videos videos={videos} />
+      )}
+    </div>
+  );
 };
 
 export default SearchFeed;
--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (searchTerm) navigate(`/search/${searchTerm}`);
+
+    setSearchTerm("");
+  };
+
   return (
     <form
       className="bg-white flex justify-center items-center rounded-[20px] border-[1px] border-[#e3e3e3] pl-2 mt-2 shadow-none sm:mr-5"
-      onSubmit={() => {}}
+      onSubmit={(e) => handleSubmit(e)}
     >
       <input
         type="text"
         className="search-bar"
-        value=""
+        value={searchTerm}
         placeholder="Search"
-        onChange={() => {}}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
 
       <button type="submit" className="p-[10px] text-[20px] m-0 text-red-500">
